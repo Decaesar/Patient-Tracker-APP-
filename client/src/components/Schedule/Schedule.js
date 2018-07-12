e016b0ffe26a27eba6b53349fe5d3545ac9e27f4
Mohammed  Fouad <moha@Mohammeds-MacBook-Pro.local>
2018-07-12 19:46:16 -0400

    upload

---
+++ client/src/components/Schedule/Schedule.js
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// const Schedule = () => (
+//         <div className="time">
+//         {numbers = [1, 2, 3, 4, 5];
+//          doubled = numbers.map((number) => number * 2)
+//          console.log(doubled);}
+
+//         </div>
+// );
+
+// export default Schedule;
